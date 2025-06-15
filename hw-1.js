3a89731517393f860bfe957f2cff3afcdfdee056
максим <notolerances@yandex.ru>
2025-06-15 14:32:32 +0300

    JS active

--- hw-1.js
+++ hw-1.js
@@ -1,4 +1,4 @@
-/*let a = 10;
+let a = 10;
 alert(a);2
 a = 20;
 alert(a);
@@ -43,4 +43,4 @@ const User = {
 }
 
 let nameUser = prompt('Как тебя зовут?');
-alert(`Привет, ${nameUser}!`);*/
+alert(`Привет, ${nameUser}!`);
